22438fda56b0a7ae64582dfa2e453ed63af1374c
Daniel Adámek <adamedan@fit.cvut.cz>
2021-09-10 22:31:58 +0200

    git ignore update

--- app.py
+++ app.py
@@ -6,20 +6,26 @@ from termcolor import colored
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-student_files = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser("files/")) for f in fn if not (is_binary(dp+"/"+f))]
+student_files = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser("files/")) for f in fn if
+                 not (is_binary(dp + "/" + f)) and not (".git") in dp and not (".gitignore" or "README.md" or ".gitconfig") in f]
 student_notes = [open(_file, encoding='utf-8').read()
                  for _file in student_files]
 
 
 def vectorize(Text): return TfidfVectorizer().fit_transform(Text).toarray()
+
+
 def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
+
 
 def round_up(n, decimals=0):
     multiplier = 10 ** decimals
     return math.ceil(n * multiplier) / multiplier
 
+
 def is_binary(f):
     return 'b' in f.mode
+
 
 vectors = vectorize(student_notes)
 s_vectors = list(zip(student_files, vectors))
@@ -40,13 +46,16 @@ def check_plagiarism():
     return plagiarism_results
 
 
+
 print("Plagiarism checker")
+log = open("./last.log", "w")
 for data in sorted(check_plagiarism(), key=lambda i: float(i[2]), reverse=True):
-    val = round_up(data[2]*100, 3)
+    val = round_up(data[2] * 100, 3)
     if val > 80:
         print(colored("[" + str('{:.2%}'.format(data[2])) + "] '" + data[0] + "' -> '" + data[1], 'red'))
     elif (val <= 80) & (val >= 60):
         print(colored("[" + str('{:.2%}'.format(data[2])) + "] '" + data[0] + "' -> '" + data[1], 'yellow'))
     else:
         print(colored("[" + str('{:.2%}'.format(data[2])) + "] '" + data[0] + "' -> '" + data[1], 'green'))
-
+    log.write("[" + str('{:.2%}'.format(data[2])) + "] '" + data[0] + "' -> '" + data[1] + "\n")
+log.close()
